685c002f2708efa8695bab1088df753f51716533
Brandon-git-hub <75026118+Brandon-git-hub@users.noreply.github.com>
2023-08-27 14:28:42 +0800

    Update img2img.py

--- scripts/img2img.py
+++ scripts/img2img.py
@@ -31,6 +31,8 @@ def load_model_from_config(config, ckpt, verbose=False):
     if "global_step" in pl_sd:
         print(f"Global Step: {pl_sd['global_step']}")
     sd = pl_sd["state_dict"]
+    
+    # from ldm.util import instantiate_from_config
     model = instantiate_from_config(config.model)
     m, u = model.load_state_dict(sd, strict=False)
     if len(m) > 0 and verbose:
