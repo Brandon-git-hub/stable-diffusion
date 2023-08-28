6c63630bcc933aeec1f400d582a09977c230dc05
Brandon-git-hub <75026118+Brandon-git-hub@users.noreply.github.com>
2023-08-28 17:17:22 +0800

    Update util.py

--- ldm/util.py
+++ ldm/util.py
@@ -82,14 +82,17 @@ def instantiate_from_config(config):
         elif config == "__is_unconditional__":
             return None
         raise KeyError("Expected key `target` to instantiate.")
+        
     return get_obj_from_str(config["target"])(**config.get("params", dict()))
 
-
+# config["target"] = 'ldm.models.diffusion.ddpm.LatentDiffusion'
 def get_obj_from_str(string, reload=False):
     module, cls = string.rsplit(".", 1)
     if reload:
         module_imp = importlib.import_module(module)
         importlib.reload(module_imp)
+    
+    # 獲取類屬性值?
     return getattr(importlib.import_module(module, package=None), cls)
 
 
